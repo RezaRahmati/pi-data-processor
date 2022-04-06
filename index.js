eb66abe47783b799288802a66fb65087a887dcc5
Reza <Reza.Rahmati@gmail.com>
2022-04-06 19:14:41 -0400

    separating the liklihood

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import { default as fetch } from 'node-fetch';
-import { default as FormData } from "form-data";
+import { default as FormData } from 'form-data';
 import { default as converter } from 'json-2-csv';
 import { globby } from 'globby';
 
@@ -16,12 +16,10 @@ dotenv.config();
         const finalPath = path.join(folder, subFolder).replace(/\\/g, '/');
         try {
             await scanFolder(finalPath);
-        }
-        catch (err) {
+        } catch (err) {
             console.error(`Error processing ${subFolder}`, err);
         }
     }
-
 })();
 
 async function scanFolder(folder) {
@@ -33,7 +31,9 @@ async function scanFolder(folder) {
         console.log(`************ ${folder} ***************`);
 
         // Get the files as an array
-        const files = (await globby([`${folder}/**/*`])).filter(f => !f.includes('cmor-result'));
+        const files = (await globby([`${folder}/**/*`])).filter(
+            (f) => !f.includes('cmor-result'),
+        );
 
         const promises = [];
 
@@ -44,7 +44,6 @@ async function scanFolder(folder) {
         console.log(`${filesCount} files found in ${folder}`);
 
         for (let file of files) {
-
             await delay(delayMs);
 
             fileIndex += 1;
@@ -65,10 +64,10 @@ async function scanFolder(folder) {
                 fetch(process.env.API_URL, {
                     method: 'POST',
                     headers: {
-                        "api-key": process.env.API_KEY,
-                        'Accept': 'application/json'
+                        'api-key': process.env.API_KEY,
+                        Accept: 'application/json',
                     },
-                    body: formData
+                    body: formData,
                 })
                     .then((res) => {
                         processedCount += 1;
@@ -77,108 +76,157 @@ async function scanFolder(folder) {
                                 return res.json();
                             } catch (err) {
                                 console.error('Error converting to json 1', res);
-                                throw new Error(res.text())
+                                throw new Error(res.text());
                             }
                         }
 
                         try {
                             return res.json();
-                        }
-                        catch (err) {
+                        } catch (err) {
                             console.error('Error converting to json 2', res);
-                            throw new Error(res.text())
+                            throw new Error(res.text());
                         }
                     })
                     .then((res) => {
                         if (res.fileName) {
-                            console.log(`Processing ${res.fileName} done. ${processedCount} of ${filesCount}`);
+                            console.log(
+                                `Processing ${res.fileName} done. ${processedCount} of ${filesCount}`,
+                            );
                         } else {
-                            console.error(`Error Processing ${file}. ${processedCount} of ${filesCount}`, 'error:', res);
+                            console.error(
+                                `Error Processing ${file}. ${processedCount} of ${filesCount}`,
+                                'error:',
+                                res,
+                            );
                         }
 
                         return res;
                     })
-                    .catch(err => {
+                    .catch((err) => {
                         console.error('Error', file, err);
-                    })
+                    }),
             );
-
         }
 
         const responses = await Promise.allSettled(promises);
 
-        const data = responses.map(r => {
-            if (r.status === 'fulfilled') {
-                const value = r.value;
+        const data = responses
+            .map((r) => {
+                if (r.status === 'fulfilled') {
+                    const value = r.value;
 
-                if (value) {
-
-                    if (value.fileName) {
-                        return {
-                            fileName: value.fileName || '',
-                            hasAnyPiData: value.stats && value.stats.length > 0 || '',
-                            fullFileName: value.fullFileName || '',
-                            fileSizeBytes: value.fileSizeBytes || '',
-                            durationSeconds: value.durationSeconds || '',
-                            data: (value.data || []).map(d => `${d.infoType || ''}:${d.data || ''}`).join(`;`) || '',
-                            stats: (value.stats || []).map(d => `${d.infoType || ''}:${d.count || ''}`).join(`;`) || '',
-                            error: '',
+                    if (value) {
+                        if (value.fileName) {
+                            return {
+                                fileName: value.fileName || '',
+                                hasAnyPiData: (value.stats && value.stats.length > 0) || '',
+                                fullFileName: value.fullFileName || '',
+                                fileSizeBytes: value.fileSizeBytes || '',
+                                durationSeconds: value.durationSeconds || '',
+                                dataVeryLikely:
+                                    (value.data || [])
+                                        .filter((d) => d.likelihood === 'VERY_LIKELY')
+                                        .map((d) => `${d.infoType || ''}:${d.data || ''}`)
+                                        .join(`;`)
+                                        .replace(/,/g, ' ') || '',
+                                dataLikely:
+                                    (value.data || [])
+                                        .filter((d) => d.likelihood === 'LIKELY')
+                                        .map((d) => `${d.infoType || ''}:${d.data || ''}`)
+                                        .join(`;`)
+                                        .replace(/,/g, ' ') || '',
+                                dataOther:
+                                    (value.data || [])
+                                        .filter(
+                                            (d) =>
+                                                d.likelihood !== 'VERY_LIKELY' &&
+                                                d.likelihood !== 'LIKELY',
+                                        )
+                                        .map(
+                                            (d) =>
+                                                `${d.infoType || ''}${!!d.likelihood ? `[${d.likelihood}]` : ''
+                                                }:${d.data || ''}`,
+                                        )
+                                        .join(`;`)
+                                        .replace(/,/g, ' ') || '',
+                                stats:
+                                    (value.stats || [])
+                                        .map((d) => `${d.infoType || ''}:${d.count || ''}`)
+                                        .join(`;`) || '',
+                                error: '',
+                            };
                         }
+
+                        return {
+                            fileName: '',
+                            hasAnyPiData: '',
+                            fullFileName: '',
+                            fileSizeBytes: '',
+                            durationSeconds: '',
+                            dataVeryLikely: '',
+                            dataLikely: '',
+                            dataOther: '',
+                            stats: '',
+                            error: JSON.stringify(value),
+                        };
                     }
 
+                    return null;
+                } else {
                     return {
                         fileName: '',
                         hasAnyPiData: '',
                         fullFileName: '',
                         fileSizeBytes: '',
                         durationSeconds: '',
-                        data: '',
+                        dataVeryLikely: '',
+                        dataLikely: '',
+                        dataOther: '',
                         stats: '',
-                        error: JSON.stringify(value)
-                    }
+                        error: r.reason,
+                    };
                 }
-
-                return null;
-            } else {
-
-                return {
-                    fileName: '',
-                    hasAnyPiData: '',
-                    fullFileName: '',
-                    fileSizeBytes: '',
-                    durationSeconds: '',
-                    data: '',
-                    stats: '',
-                    error: r.reason
-                }
-            }
-        }).filter(o => !!o);
+            })
+            .filter((o) => !!o);
 
         const dateTime = getDate();
 
         const csvData = await converter.json2csvAsync(data);
         fs.writeFileSync(path.join(folder, `cmor-result-${dateTime}.csv`), csvData);
 
-        const allSuccessfulFiles = data.map(d => d.fullFileName);
-        const allUnsuccessfulFiles = files.filter(f => !allSuccessfulFiles.includes(f));
+        const allSuccessfulFiles = data.map((d) => d.fullFileName);
+        const allUnsuccessfulFiles = files.filter(
+            (f) => !allSuccessfulFiles.includes(f),
+        );
         if (allUnsuccessfulFiles && allUnsuccessfulFiles.length) {
-            const allUnsuccessfulFilesCsv = await converter.json2csvAsync(allUnsuccessfulFiles.map(f => ({ fullFileName: f })));
-            fs.writeFileSync(path.join(folder, `cmor-result-missing-files-${dateTime}.csv`), allUnsuccessfulFilesCsv);
+            const allUnsuccessfulFilesCsv = await converter.json2csvAsync(
+                allUnsuccessfulFiles.map((f) => ({ fullFileName: f })),
+            );
+            fs.writeFileSync(
+                path.join(folder, `cmor-result-missing-files-${dateTime}.csv`),
+                allUnsuccessfulFilesCsv,
+            );
         }
 
         const end = new Date();
 
-        console.log(`Processing ${filesCount} files done in ${Math.floor((end - start) / 1000 / 60)} minutes`)
-    }
-    catch (e) {
-        console.error("Whoops!", e);
+        console.log(
+            `Processing ${filesCount} files done in ${Math.floor(
+                (end - start) / 1000 / 60,
+            )} minutes`,
+        );
+    } catch (e) {
+        console.error('Whoops!', e);
     }
 }
 
 const getDate = () => {
-    return new Date().toLocaleString().replace(/[T,]/gi, '').replace(/[\/: ]/gi, '-');
-}
+    return new Date()
+        .toLocaleString()
+        .replace(/[T,]/gi, '')
+        .replace(/[\/: ]/gi, '-');
+};
 
 const delay = (ms) => {
     return new Promise((resolve, reject) => setTimeout(resolve, ms));
-}
+};
